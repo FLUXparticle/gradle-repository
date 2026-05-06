8d8bd829e2b33b1229b39a9927e71edba9f3bffe
Sven Reinck <sreinck@fluxparticle.de>
2026-05-06 07:18:12 +0200

    Aufgabe 4: Prüfsummen für alle JARs erzeugen

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,24 @@
+plugins {
+    id("org.gradle.crypto.checksum") version "1.4.0" apply false
+}
+
 subprojects {
     repositories {
         mavenCentral()
+    }
+
+    plugins.withId("java") {
+        apply(plugin = "org.gradle.crypto.checksum")
+
+        val jar by tasks.getting
+
+        val createChecksums by tasks.registering(org.gradle.crypto.checksum.Checksum::class) {
+            dependsOn(jar)
+            inputFiles = jar.outputs.files
+            outputDirectory = layout.buildDirectory.dir("checksums")
+            checksumAlgorithm.set(org.gradle.crypto.checksum.Checksum.Algorithm.MD5)
+        }
+
+        jar.finalizedBy(createChecksums)
     }
 }
